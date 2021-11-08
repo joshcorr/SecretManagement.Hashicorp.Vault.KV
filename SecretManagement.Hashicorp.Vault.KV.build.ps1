6bb9f80e949928a0a9f1638c94c49f7c96af1d93
Josh Corrick <joshcorr@users.noreply.github.com>
2021-11-07 21:17:31 -0500

    Bumping to 1.2.0

--- SecretManagement.Hashicorp.Vault.KV.build.ps1
+++ SecretManagement.Hashicorp.Vault.KV.build.ps1
@@ -6,7 +6,7 @@ task copy {
     $Version = (Test-ModuleManifest -Path "$PSScriptRoot\$ModuleName\$ModuleName.psd1").Version
     $UserModulePath = [Environment]::GetEnvironmentVariable('PSModulePath') -split [IO.Path]::PathSeparator | Where-Object {$PSItem -notmatch [Environment]::UserName} | Select-Object -First 1
     $Destination = $UserModulePath, $ModuleName, $Version -join [io.path]::DirectorySeparatorChar
-    $Source = $PSScriptRoot, $ModuleName, '*' -join [io.path]::DirectorySeparatorChar
+    $Source = $PSScriptRoot, $ModuleName, ' ' -join [io.path]::DirectorySeparatorChar
     Copy-Item -Path $Source -Destination $Destination -Force -Recurse -Verbose
 }
 
