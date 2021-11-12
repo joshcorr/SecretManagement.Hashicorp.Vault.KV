86531e2d9498da328ac1e35a88f1399c64fc5ead
Josh Corrick <joshcorr@users.noreply.github.com>
2021-11-11 21:53:26 -0500

    Impove Build Script

--- SecretManagement.Hashicorp.Vault.KV.build.ps1
+++ SecretManagement.Hashicorp.Vault.KV.build.ps1
@@ -12,4 +12,11 @@ task copy {
     Copy-Item -Path $Source -Destination $Destination -Force -Recurse -Verbose
 }
 
+task remove {
+    $Version = (Test-ModuleManifest -Path "$PSScriptRoot\$ModuleName\$ModuleName.psd1").Version
+    $UserModulePath = [Environment]::GetEnvironmentVariable('PSModulePath') -split [IO.Path]::PathSeparator | Where-Object {$PSItem -notmatch [Environment]::UserName} | Select-Object -First 1
+    $Destination = $UserModulePath, $ModuleName, $Version -join [io.path]::DirectorySeparatorChar
+    Remove-Item -Path $Destination -Force -Recurse -Verbose
+}
+
 task . copy
